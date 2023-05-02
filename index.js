db36a22f7cadfc891e7cb05559bbab49f8b050f4
emon1995 <emonhossain995@gmail.com>
2023-05-02 20:48:49 +0600

    recipe routes added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const comments = require("./data/comment.json");
 const chefs = require("./data/chef.json");
+const recipes = require("./data/recipe.json");
 
 const app = express();
 const PORT = 5000;
@@ -25,6 +26,12 @@ app.get("/chefs/:id", (req, res) => {
   const id = req.params.id;
   const selectedChef = chefs.find((chef) => chef.id === parseInt(id));
   res.json(selectedChef);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedRecipe = recipes.filter((recipe) => recipe.id === id);
+  res.json(selectedRecipe);
 });
 
 app.listen(PORT, () => {
